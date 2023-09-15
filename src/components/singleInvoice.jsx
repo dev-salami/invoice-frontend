f91b0ff5b5e8c08c0bb9faa9dd8612c3d5954f8f
khalil <salamikhalil02@gmail.com>
2023-09-15 11:31:06 +0100

    single invoice bg change

--- src/components/singleInvoice.jsx
+++ src/components/singleInvoice.jsx
@@ -40,11 +40,11 @@ function SingleInvoice({ invoice }) {
 					animate={{ x: 0 }}
 					exit={{ x: "200%" }}
 					transition={{ duration: 0.5 }}
-					className="dark:bg-[#141625] mx-auto duration-300 min-h-screen bg-[#f8f8fb] py-[34px] px-2 md:px-8 lg:px-12 max-w-3xl lg:py-[72px] ">
+					className="bg-[#141625] mx-auto duration-300 min-h-screen  py-[34px] px-2 md:px-8 lg:px-12 max-w-3xl lg:py-[72px] ">
 					<div className="">
 						<button
 							// onClick={() => router.back()}
-							className=" flex items-center space-x-4  group  dark:text-white font-thin ">
+							className=" flex items-center space-x-4  group  text-white font-thin ">
 							{/* <img className='' src={leftArrow} /> */}
 							<Link
 								href="/invoices"
@@ -53,15 +53,15 @@ function SingleInvoice({ invoice }) {
 							</Link>
 						</button>
 
-						<div className=" mt-8 rounded-lg w-full flex items-center justify-between px-6 py-6 bg-white dark:bg-[#1e2139]">
+						<div className=" mt-8 rounded-lg w-full flex items-center justify-between px-6 py-6  bg-[#1e2139]">
 							<div className=" flex space-x-2 justify-between md:justify-start md:w-auto w-full items-center">
-								<h1 className=" text-gray-600 dark:text-gray-400">Status</h1>
+								<h1 className="  text-gray-400">Status</h1>
 								<PaidStatus type={invoice?.status} />
 							</div>
 							<div className=" md:block hidden">
 								<button
 									onClick={() => dispatch(toggleEditModal(true))}
-									className=" text-[#7e88c3] text-center dark:bg-[#252945] hover:opacity-80  bg-slate-100 p-3 px-7 rounded-full ">
+									className=" text-[#7e88c3] text-center bg-[#252945] hover:opacity-80   p-3 px-7 rounded-full ">
 									Edit
 								</button>
 								<button
@@ -79,10 +79,10 @@ function SingleInvoice({ invoice }) {
 							</div>
 						</div>
 
-						<div className=" mt-4 rounded-lg w-full  px-6 py-6 bg-white dark:bg-[#1e2139]">
+						<div className=" mt-4 rounded-lg w-full  px-6 py-6  bg-[#1e2139]">
 							<div className=" flex flex-col md:flex-row items-start justify-between w-full ">
 								<div>
-									<h1 className=" font-semibold dark:text-white text-xl">
+									<h1 className=" font-semibold text-white text-xl">
 										<span className="text-[#7e88c3]">#</span>
 										{invoice.id}
 									</h1>
@@ -102,7 +102,7 @@ function SingleInvoice({ invoice }) {
 										<h3 className=" text-gray-400 font-thin mb-2  ">
 											Invoice Date
 										</h3>
-										<h1 className="  font-semibold dark:text-white">
+										<h1 className="  font-semibold text-white">
 											{formatDBDate(invoice.createdAt)}
 										</h1>
 									</div>
@@ -110,7 +110,7 @@ function SingleInvoice({ invoice }) {
 										<h3 className=" text-gray-400 font-thin mb-2">
 											Payment Due
 										</h3>
-										<h1 className=" dark:text-white  font-semibold">
+										<h1 className=" text-white  font-semibold">
 											{formatDBDate(invoice.paymentDue)}
 										</h1>
 									</div>
@@ -118,7 +118,7 @@ function SingleInvoice({ invoice }) {
 
 								<div className="">
 									<p className=" text-gray-400 font-thin mb-2">Bill to</p>
-									<h1 className=" dark:text-white  font-semibold">
+									<h1 className=" text-white  font-semibold">
 										{invoice.clientName}
 									</h1>
 									<p className=" text-gray-400 font-thin">
@@ -137,7 +137,7 @@ function SingleInvoice({ invoice }) {
 
 								<div className=" mt-8 md:mt-0">
 									<p className=" text-gray-400 font-thin mb-2">Sent to</p>
-									<h1 className=" dark:text-white  font-semibold">
+									<h1 className=" text-white  font-semibold">
 										{invoice.clientEmail}
 									</h1>
 								</div>
@@ -145,18 +145,18 @@ function SingleInvoice({ invoice }) {
 
 							{/* Last Section */}
 
-							<div className=" sm:hidden mt-10 bg-[#f9fafe] dark:bg-[#252945] rounded-lg rounded-b-none space-y-4  p-10">
+							<div className=" sm:hidden mt-10  bg-[#252945] rounded-lg rounded-b-none space-y-4  p-10">
 								{invoice.items.map((item, index) => (
 									<div
 										key={index}
-										className=" justify-between  dark:text-white flex">
+										className=" justify-between  text-white flex">
 										<h1>{item.name}</h1>
 										<h1>£{item.total}</h1>
 									</div>
 								))}
 							</div>
 
-							<div className=" hidden sm:block mt-10 bg-[#f9fafe] dark:bg-[#252945] rounded-lg rounded-b-none space-y-4  p-10">
+							<div className=" hidden sm:block mt-10  bg-[#252945] rounded-lg rounded-b-none space-y-4  p-10">
 								{invoice.items.map((item) => (
 									<div
 										key={item.name}
@@ -164,35 +164,35 @@ function SingleInvoice({ invoice }) {
 										<div className=" space-y-4">
 											<p className=" text-gray-400 font-thin">Item name</p>
 
-											<h1 className=" dark:text-white text-base font-semibold">
+											<h1 className=" text-white text-base font-semibold">
 												{item.name}
 											</h1>
 										</div>
 										<div className=" space-y-4">
 											<p className=" text-gray-400 font-thin">Qty.</p>
 
-											<h1 className=" dark:text-white text-base font-semibold">
+											<h1 className=" text-white text-base font-semibold">
 												{item.quantity}
 											</h1>
 										</div>
 										<div className=" space-y-4">
 											<p className=" text-gray-400 font-thin">Item price</p>
 
-											<h1 className=" dark:text-white text-base font-semibold">
+											<h1 className=" text-white text-base font-semibold">
 												£{item.price}
 											</h1>
 										</div>
 										<div className=" space-y-4">
 											<p className=" text-gray-400 font-thin">Total</p>
 
-											<h1 className=" dark:text-white text-base font-semibold">
+											<h1 className=" text-white text-base font-semibold">
 												£{item.total}
 											</h1>
 										</div>
 									</div>
 								))}
 							</div>
-							<div className=" p-10 font-semibold text-white rounded-lg rounded-t-none justify-between flex dark:bg-black bg-gray-700 ">
+							<div className=" p-10 font-semibold text-white rounded-lg rounded-t-none justify-between flex  bg-gray-700 ">
 								<h3 className=" text-xl ">Amount Due</h3>
 
 								<h1 className=" text-3xl">£{invoice.total}</h1>
